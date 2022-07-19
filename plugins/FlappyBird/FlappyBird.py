e10ca6497dd93d6b980a0116834a50a58809aa7f
snowfelix <45322039+snowfelix@users.noreply.github.com>
2022-07-19 10:11:12 +0200

    yallah mini baby debug fix 16

--- plugins/FlappyBird/FlappyBird.py
+++ plugins/FlappyBird/FlappyBird.py
@@ -88,7 +88,7 @@ class FlappyBird(BasePlugin):
 
 
     def gravity(self):
-        if self.fall_duration > 4:
+        if self.fall_duration < 4:
             self.fall_duration += 1
             print("yallah gravity")
         current_position = self.getCurrentPlayerPosition()
