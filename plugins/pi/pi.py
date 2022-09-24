c8c05786729589edba353135783cfbb22cf3ed15
piLovr <95534694+piLovr@users.noreply.github.com>
2022-09-24 15:22:28 +0200

    Create pi.py

---
+++ plugins/pi/pi.py
@@ -0,0 +1,60 @@
+from ..basePlugin import BasePlugin
+from decimal import getcontext, Decimal
+
+class Tetrix(BasePlugin):
+    def __init__(self, app, output):
+        super().__init__(app, output)
+        self.pluginName = "Pi"
+        self.version = "3.14"
+
+    def main(num_Digits):
+        n = 0
+        Pi = 0
+        global Pi, n
+        getcontext().prec = num_Digits+2
+        n = 0
+        a = 1
+        b = 1/Decimal(2).sqrt()
+        s = 1/Decimal(4)
+        run = True
+
+        while run:
+            if print_n:
+                print(n)
+            A = (a+b)/2
+            B = Decimal(a*b).sqrt()
+            S = s - 2**n * (a-A)**2
+
+
+
+            if a==A:
+                Pi = (str(A**2/s)[:-1])
+                return Pi
+
+
+            a = A
+            b = B
+            s = S
+
+            n=n+1
+        
+
+
+    def run(self):
+        pi = list(str(self.main(30*15)))
+        x = 0
+        pixelBoard = [[0 for i in range(30)] for j in range(15)]
+        for a in range(30):
+            for b in range(15):
+                pixelboard[a][b] = pi[x]
+                x += 1
+        self.out.set_matrix(pixelBoard)
+        self.out.submit_all()
+                
+        
+
+    def input(self, inp):
+        pass
+    
+    def get_html(self):
+        return ""
