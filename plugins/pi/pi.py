944b3bc3b2f112bf0fedecb5eaa31ad22e5c1115
piLovr <95534694+piLovr@users.noreply.github.com>
2022-09-24 16:28:06 +0200

    Update pi.py

--- plugins/pi/pi.py
+++ plugins/pi/pi.py
@@ -66,7 +66,7 @@ class Tetrix(BasePlugin):
         pixelBoard = [[0 for i in range(15)] for j in range(30)]
         for a in range(30):
             for b in range(15):
-                pixelBoard[b][a] = self.getColor(int(pi[x+2]))
+                pixelBoard[a][b] = self.getColor(int(pi[x+2]))
                 x += 1
         print(pixelBoard)
         self.out.set_matrix(pixelBoard)
